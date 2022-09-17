45da5699f564882cddc367dc82944344bef97bca
dsad1fs <113667485+dsad1fs@users.noreply.github.com>
2022-09-17 16:22:40 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -139,13 +139,13 @@ export const config = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "",
+  CALLBACK_TEMPLATE_ID: "ZQQ3xe9BjNVmRe-1CSTw3_a_2O_PkNd939G3tqozVfc",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
     {
       // 一般都填自己
-      name: "自己",
+      name: "why4817",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "o4Ak26kgIWVg_6yvGZGghHPuFDEM",
     },
